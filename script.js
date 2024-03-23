532f11d1d3b850e0284a113095891fe98ac3fe98
Tanzeel <tanzeelmughal431@gmail.com>
2024-03-23 22:54:32 +0500

    practices

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-document.write("HEllo World")
+document.write("HEllo World");
 
-function doo(){
-    
+function doo() {
+  let targetbody = document.getElementsByTagName("body");
+  document.body.innerHTML = "";
+  console.log(targetbody);
+  let buton = document.createElement("button");
+  buton.innerText = "This for clear All";
 }
