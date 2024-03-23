22c414ce84d08bd256b10dd82321332b0d3e1539
Tanzeel <tanzeelmughal431@gmail.com>
2024-03-23 22:56:37 +0500

    add the changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.write("HEllo World");
 
 function doo() {
-  let targetbody = document.getElementsByTagName("body");
-  document.body.innerHTML = "";
+  let targetbody = document.getElementById("contatiner");
+  targetbody.innerHTML = "";
   console.log(targetbody);
   let buton = document.createElement("button");
   buton.innerText = "This for clear All";
